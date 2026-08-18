a313953cfc5b30ffb076208e5ebae2287623163e
Dmytro Larin <d.larin@mascode.com>
2026-08-18 14:07:21 +0300

    Remove useless interface

--- src/roles/user-roles.model.ts
+++ src/roles/user-roles.model.ts
@@ -2,11 +2,6 @@ import {Column, DataType, ForeignKey, Model, Table} from 'sequelize-typescript';
 import {User} from "../users/users.model";
 import {Role} from "./roles.model";
 
-interface RoleCreationAttrs {
-    value: string;
-    description: string;
-}
-
 @Table({tableName: 'user_roles', createdAt: false, updatedAt: false})
 export class UserRoles extends Model<UserRoles> {
     @Column({type: DataType.INTEGER, unique: true, autoIncrement: true, primaryKey: true})
